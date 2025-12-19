92887113e23c49956017f55435a48256698a11a7
a <102840087+kingdudely@users.noreply.github.com>
2025-12-19 15:55:32 -0500

    Refactor CORS handling and request fetching logic

--- worker.js
+++ worker.js
@@ -1,26 +1,26 @@
 export default {
 	async fetch(request) {
-		if (request.method === "OPTIONS" && request.headers.has("Access-Control-Request-Method")) {
-			return new Response(null, {
-				status: 204,
-				headers: {
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Methods": "*",
-					"Access-Control-Allow-Headers": "*",
-				}
-			});
-		}
+		try {
+			if (request.method === "OPTIONS" && request.headers.has("Access-Control-Request-Method")) {
+				return new Response(null, {
+					status: 204,
+					headers: {
+						"Access-Control-Allow-Origin": "*",
+						"Access-Control-Allow-Methods": "*",
+						"Access-Control-Allow-Headers": "*",
+					}
+				});
+			}
+	
+			const searchParams = new URL(request.url).searchParams;
+			const corsUrl = new URL(decodeURIComponent(searchParams.get("url")));
 
-		const url = new URL(request.url);
-		const urlParam = url.searchParams.get("url");
-
-		if (!urlParam) {
-			return new Response("Invalid ?url=...", {
+			const newRequest = new Request(corsUrl, request);
+			return fetch(newRequest); // await?
+		} catch (error) {
+			return new Response(error, {
 				status: 400
 			});
 		}
-
-		const newRequest = new Request(urlParam, request)
-		return await fetch(newRequest);
 	}
 }
