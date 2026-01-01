5ec5a41811242ebab372d6dc83584ae9d7915bc8
a <102840087+kingdudely@users.noreply.github.com>
2025-12-31 21:02:29 -0500

    Refactor CORS handling in fetch function

--- worker.js
+++ worker.js
@@ -1,26 +1,37 @@
-export default {
-	async fetch(request) {
-		try {
-			if (request.method === "OPTIONS" && request.headers.has("Access-Control-Request-Method")) {
-				return new Response(null, {
-					status: 204,
-					headers: {
-						"Access-Control-Allow-Origin": "*",
-						"Access-Control-Allow-Methods": "*",
-						"Access-Control-Allow-Headers": "*",
-					}
-				});
-			}
-	
-			const searchParams = new URL(request.url).searchParams;
-			const corsUrl = new URL(decodeURIComponent(searchParams.get("url")));
+function addCorsHeaders(response) {
+    const newResponse = new Response(response.body, response);
+    const newHeaders = newResponse.headers;
+    newHeaders.set("Access-Control-Allow-Origin", "*");
+    newHeaders.set("Access-Control-Allow-Methods", "*");
+    newHeaders.set("Access-Control-Allow-Headers", "*");
 
-			const newRequest = new Request(corsUrl, request);
-			return fetch(newRequest); // await?
-		} catch (error) {
-			return new Response(error, {
-				status: 400
-			});
-		}
-	}
+    return newResponse;
+}
+
+export default {
+    async fetch(request) {
+        try {
+            if (request.method === "OPTIONS" && request.headers.has("Access-Control-Request-Method")) {
+                return addCorsHeaders(new Response(null, {
+                    status: 204
+                }));
+            }
+    
+            const urlParam = new URL(request.url).searchParams.get("url");
+            if (!urlParam) {
+                return addCorsHeaders(new Response("Missing '?url=...' URL parameter", {
+                    status: 400
+                }));
+            }
+            const targetUrl = new URL(decodeURIComponent(urlParam));
+            const newRequest = new Request(targetUrl, request);
+            const response = addCorsHeaders(await fetch(newRequest));
+
+            return response;
+        } catch (error) {
+            return addCorsHeaders(new Response(error, {
+                status: 500
+            }));
+        }
+    }
 }
